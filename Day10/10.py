ab07a56f69905147c11b01915696df2b83b6e79c
Jim Gridley <jgridley@fastmail.com>
2021-12-13 00:44:22 -0800

    Day 10 part 2

--- Day10/10.py
+++ Day10/10.py
@@ -1,8 +1,10 @@
+# Part 2
+
 scores = {
-    ")" : 3,
-    "]" : 57,
-    "}" : 1197,
-    ">" : 25137,
+    "(" : 1,
+    "[" : 2,
+    "{" : 3,
+    "<" : 4,
 }
 
 matches = {
@@ -12,17 +14,59 @@ matches = {
     ">" : "<",
 }
 
-score = 0
+lineScores = []
 with open("input10") as f:
     for line in f:
         stack = []
+        next = False
         for char in line.strip():
-            if char in scores:
-                if stack[-1] != matches[char]:
-                    score += scores[char]
-                    break
-                else:
+            if char in matches:
+                if stack[-1] == matches[char]:
                     stack.pop()
+                else:
+                    next = True
+                    break
             else:
                 stack.append(char)
-print(score)
+        if next:
+            continue
+        score = 0
+        while stack:
+            char = stack.pop()
+            if char not in matches:
+                score *= 5
+                score += scores[char]
+        lineScores.append(score)
+
+lineScores = sorted(lineScores)
+print(lineScores[len(lineScores)//2])
+
+
+# # Part 1
+# scores = {
+#     ")" : 3,
+#     "]" : 57,
+#     "}" : 1197,
+#     ">" : 25137,
+# }
+#
+# matches = {
+#     ")" : "(",
+#     "]" : "[",
+#     "}" : "{",
+#     ">" : "<",
+# }
+# score = 0
+# with open("input10") as f:
+#     for line in f:
+#         stack = []
+#         for char in line.strip():
+#             if char in scores:
+#                 if stack[-1] != matches[char]:
+#                     score += scores[char]
+#                     break
+#                 else:
+#                     stack.pop()
+#             else:
+#                 stack.append(char)
+# print(score)
