9d7c668b5350c60ad1ec0084a9590e537145a6d6
Jim Gridley <jgridley@fastmail.com>
2021-12-17 01:12:36 -0800

    Day 15 part 2

--- Day15/15.py
+++ Day15/15.py
@@ -1,3 +1,43 @@
+from heapq import heappush, heappop
+
+# Part 2
+
+grid = []
+
+with open("input15") as f:
+    for line in f:
+        grid.append(list(map(int, list(line.strip()))))
+
+numRows = len(grid)*5
+numCols = len(grid[0])*5
+heap = []
+
+curr = (0, (0, 0))
+heappush(heap, curr)
+inHeap = {(0,0)}
+
+
+while curr[1] != (numRows-1, numCols-1):
+    for di, dj in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+        ni = di+curr[1][0]
+        nj = dj+curr[1][1]
+        if ni >= numRows or ni < 0 or nj >= numCols or nj < 0 or (ni, nj) in inHeap:
+            continue
+        newEdge = grid[ni%len(grid)][nj%len(grid[0])] + ni//len(grid) + nj//len(grid[0])
+        if newEdge > 9:
+            newEdge = newEdge % 10 + 1
+        # if ni >= len(grid) or nj >= len(grid[0]):
+        #     print(newEdge, ni, nj)
+        #     assert False
+        heappush(heap, (curr[0] + newEdge, (ni, nj)))
+        inHeap.add((ni, nj))
+    curr = heappop(heap)
+
+print(curr[0])
+
+
+
+
 
 
 # # Part 1
